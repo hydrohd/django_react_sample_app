15ac2cf84ccc855f7af12fc0e1a965cddc78e657
hydrohd <eaescob3@asu.edu>
2024-06-23 10:23:31 -0700

    add basic views for quiz, question & answer. WIP

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from rest_framework  import generics, status
+from rest_framework.response import Response
+from .models import Quiz, Question, Answer
+
+from api.serializers.answer_serializer import AnswerSerializer
+from api.serializers.question_serializer import QuestionSerializer
+from api.serializers.quiz_serializer import QuizSerializer
 
 # Create your views here.
+
+class ListCreateQuiz(generics.ListCreateAPIView):
+    queryset = Quiz.objects.all()
+    serializer_class = QuizSerializer
+
+class RetrieveUpdateDestroyQuiz(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Quiz.objects.all()
+    serializer_class = QuizSerializer
+
+
+class ListCreateQuestion(generics.ListCreateAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+
+class RetrieveUpdateDestroyQuestion(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+
+class ListCreateAnswer(generics.ListCreateAPIView):
+    queryset = Answer.objects.all()
+    serializer_class = AnswerSerializer
+
+class RetrieveUpdateDestroyAnswer(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Answer.objects.all()
+    serializer_class = AnswerSerializer
