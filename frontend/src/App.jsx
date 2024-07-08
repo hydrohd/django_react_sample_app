17714149d20a48540620037b42ce36ce66335a1c
hydrohd <eddy@edesc.dev>
2024-07-07 19:06:54 -0700

    messing around with daisyui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,41 @@
 import { useState } from 'react'
 
 import './App.css'
+import Nav from './components/Nav.jsx'
+import Footer from './components/Footer.jsx'
 
 function App() {
-  return (
-    <>
-        <h1 className='text-blue-700 text-center'>Django React Sample App</h1>
-    </>
-  )
+    return (
+        <>
+            <header className='fixed top-0 w-full backdrop-blur-sm'>
+                <Nav />
+            </header>
+
+            <main>
+                <div
+                    className="hero min-h-screen"
+                    style={{
+                        backgroundImage: "url(https://i.imgur.com/lDb3gOZ.jpeg)",
+                    }}>
+
+                    <div className="hero-overlay bg-opacity-60"></div>
+                    <div className="hero-content text-neutral-content text-center">
+                        <div className="max-w-md">
+                            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+                            <p className="mb-5">
+                                Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                                quasi. In deleniti eaque aut repudiandae et a id nisi.
+                            </p>
+                            <button className="btn btn-primary">Get Started</button>
+                        </div>
+                    </div>
+
+                </div>
+            </main>
+                
+            <Footer></Footer>
+        </>
+    )
 }
 
 export default App
